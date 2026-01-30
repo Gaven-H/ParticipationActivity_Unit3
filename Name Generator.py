5b884418d5a53e71a21a08ddd9d74098d769e25d
Bemst <gahuforschool@gmail.com>
2026-01-29 22:32:23 -0500

    Add dragon title list

--- Name Generator.py
+++ Name Generator.py
@@ -1,2 +1,4 @@
 dragon_first_name = ["Elbar", "Nyzass", "Yghut", "Medig", "Julba", "Rogyt", "Chrosym", "Birgiantyrth", "Rinnurryrth", "Tirsocrainth"]
 
+dragon_title = ["The Fierce", "The Voiceless One", "Champion of The Red", "Longtail", "The Magnificent", "Champion of The Skies", "The Bunny Killer", "The White One", "Lord of The White", "Destroyer Of Life"]
+
