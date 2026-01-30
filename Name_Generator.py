22ab0bae6fca1423f405b4270cc1525a6880c88b
Bemst <gahuforschool@gmail.com>
2026-01-29 22:40:07 -0500

    for loop to print out dragon names

--- Name_Generator.py
+++ Name_Generator.py
@@ -2,3 +2,5 @@ dragon_first_name = ["Elbar", "Nyzass", "Yghut", "Medig", "Julba", "Rogyt", "Chr
 
 dragon_title = ["The Fierce", "The Voiceless One", "Champion of The Red", "Longtail", "The Magnificent", "Champion of The Skies", "The Bunny Killer", "The White One", "Lord of The White", "Destroyer Of Life"]
 
+for item in dragon_first_name:
+    print(f"{dragon_first_name}, {dragon_title}")
