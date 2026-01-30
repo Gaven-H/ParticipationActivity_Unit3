cd3f42454844d7f7f6445804ea4ed8139a799da7
Bemst <gahuforschool@gmail.com>
2026-01-29 22:51:09 -0500

    Replace for loop with while loop

--- Name_Generator.py
+++ Name_Generator.py
@@ -2,5 +2,5 @@ dragon_first_name = ["Elbar", "Nyzass", "Yghut", "Medig", "Julba", "Rogyt", "Chr
 
 dragon_title = ["The Fierce", "The Voiceless One", "Champion of The Red", "Longtail", "The Magnificent", "Champion of The Skies", "The Bunny Killer", "The White One", "Lord of The White", "Destroyer Of Life"]
 
-for item in dragon_first_name:
-    print(f"{dragon_first_name}, {dragon_title}")
+while dragon_first_name and dragon_title:
+    print (f"{dragon_first_name[0]}, {dragon_title[0]}")
